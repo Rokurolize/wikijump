b03870f3207765646e6d439924cc1b0adc6d3362
Rokurolize <1701388+Rokurolize@users.noreply.github.com>
2026-09-01 10:37:15 +0900

    chore(ci): restore framerail lint baseline

--- framerail/tests/manage-site-general-script-transport.test.js
+++ framerail/tests/manage-site-general-script-transport.test.js
@@ -3,7 +3,16 @@ import test from "node:test"
 import { readFile } from "node:fs/promises"
 
 test("ManageSiteGeneral does not advertise plaintext active script", async () => {
-  const source = await readFile(new URL("../src/routes/ajax-module-connector.php/+server.ts", import.meta.url), "utf8")
-  assert.match(source, /https:\/\/d3g0gp89917ko0\.cloudfront\.net\/v--7690939296dc\/common--modules\/js\/managesite\/ManageSiteGeneralModule\.js/u)
-  assert.doesNotMatch(source, /http:\/\/d3g0gp89917ko0\.cloudfront\.net\/v--7690939296dc\/common--modules\/js\/managesite\/ManageSiteGeneralModule\.js/u)
+  const source = await readFile(
+    new URL("../src/routes/ajax-module-connector.php/+server.ts", import.meta.url),
+    "utf8"
+  )
+  assert.match(
+    source,
+    /https:\/\/d3g0gp89917ko0\.cloudfront\.net\/v--7690939296dc\/common--modules\/js\/managesite\/ManageSiteGeneralModule\.js/u
+  )
+  assert.doesNotMatch(
+    source,
+    /http:\/\/d3g0gp89917ko0\.cloudfront\.net\/v--7690939296dc\/common--modules\/js\/managesite\/ManageSiteGeneralModule\.js/u
+  )
 })
